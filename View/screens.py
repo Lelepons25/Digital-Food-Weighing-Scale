ea954ecb80ea6a57c8bce26f1005c1671221fc8c
Lyll Kristine Cabelin <lyllkris@gmail.com>
2023-04-04 15:57:30 +0800

    Separated the screen oages

---
+++ View/screens.py
@@ -0,0 +1,8 @@
+screens ={
+    "Splash": "SplashScreen_Page.SplashScreenPage",
+    "Profile": "Profile_Page.ProfilePage",
+    "MealPlan": "MealPlan_Page.MealPlanPage",
+    "Home": "Home_page.Homepage",
+    "Category": "Category_Page.CategoryPage",
+    "EditProfile": "EditProfile_Page.EditProfilePage"
+}
