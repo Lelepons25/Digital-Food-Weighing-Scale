0fa9e08b8140b0d3d3a8a80213b75857f6176766
Lyll Kristine Cabelin <lyllkris@gmail.com>
2023-04-10 23:01:47 +0800

    Edit: Can display the buttons

--- View/Category_Page/CategoryPage.py
+++ View/Category_Page/CategoryPage.py
@@ -1,12 +1,19 @@
 from kivy.uix.screenmanager import Screen
 from kivymd.uix.list import TwoLineListItem
 from kivy.lang import Builder
+from kivy.uix.button import Button
 
 Builder.load_file('View\Category_Page\CategoryPage.kv')
 
 class CategoryPage(Screen):
-    def foodList(self):
-        for i in range(20):
-            item = TwoLineListItem(text = str(i) + ' item',
-                                    secondary_text = '2nd ' + str(i) + 'th item')
-            self.ids.food.add_widget(item)
+    # Edit: Di mudisplay ang list
+    def on_pre_enter(self, *args):
+        for i in range(0, 100):
+            foodList = Button(text = str(i+1), 
+                       size_hint_y = (None),
+                       size = ("50dp", "50dp"))
+            self.ids.food.add_widget(foodList)
+    
+    def reset(self):
+        self.ids.food.clear_widgets()
+        self.manager.current = "Homepage"
