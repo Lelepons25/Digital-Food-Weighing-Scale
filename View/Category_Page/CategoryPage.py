077de34a4d3877f97b9288c3f3fbe3d23fd8cc8a
Lyll Kristine Cabelin <lyllkris@gmail.com>
2023-04-12 14:01:05 +0800

    Edit: Button Bind Press

--- View/Category_Page/CategoryPage.py
+++ View/Category_Page/CategoryPage.py
@@ -6,14 +6,20 @@ from kivy.uix.button import Button
 Builder.load_file('View\Category_Page\CategoryPage.kv')
 
 class CategoryPage(Screen):
-    # Edit: Di mudisplay ang list
+
     def on_pre_enter(self, *args):
+
+        # check if the button is pressed
+        def presser(self):
+            print("Pressed")
+
         for i in range(0, 100):
             foodList = Button(text = str(i+1), 
                        size_hint_y = (None),
                        size = ("50dp", "50dp"))
-            self.ids.food.add_widget(foodList)
-    
+            foodList.bind(on_press = presser)
+            self.ids.foodList.add_widget(foodList)
+            
     def reset(self):
         self.ids.food.clear_widgets()
         self.manager.current = "Homepage"
