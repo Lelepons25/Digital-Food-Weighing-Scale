db45ad5f37cad3206c31d7d00b7940c2f1635381
Lyll Kristine Cabelin <lyllkris@gmail.com>
2023-04-13 23:40:41 +0800

    New: Complete Categories

--- View/More_Page/MorePage.py
+++ View/More_Page/MorePage.py
@@ -2,5 +2,7 @@ from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
 
 Builder.load_file('View\More_Page\MorePage.kv')
+
+# More Page displays the other categories
 class MorePage(Screen):
     pass
