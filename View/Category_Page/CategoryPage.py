a3436d1d3ca24f83e0efb7b6627ca018ff5686e1
Lyll Kristine Cabelin <lyllkris@gmail.com>
2023-04-15 18:17:55 +0800

    Added: DB Connected

--- View/Category_Page/CategoryPage.py
+++ View/Category_Page/CategoryPage.py
@@ -3,8 +3,14 @@ from kivymd.uix.list import TwoLineListItem
 from kivy.lang import Builder
 from kivy.uix.button import Button
 
+import pandas as pd
+import sqlite3
+#import ConnectDB
 
 Builder.load_file('View\Category_Page\CategoryPage.kv')
+
+conn = sqlite3.connect('food_mixtures.db')
+#conn = ConnectDB.connectFoodMix()
 
 class CategoryPage(Screen):
     
@@ -14,12 +20,22 @@ class CategoryPage(Screen):
         def presser(self):
             print("Pressed")
 
-        for i in range(0, 100):
-            foodList = Button(text = str(i+1), 
-                       size_hint_y = (None),
-                       size = ("50dp", "50dp"))
+        cursor = conn.execute("SELECT foodName FROM FoodMixturesTable")
+
+        data = []
+        
+        for row in cursor:
+            data.append(row[0])
+
+        for item in data:
+            foodList = Button(text = item, 
+                    size_hint_y = (None),
+                    size = ("50dp", "50dp"))
             foodList.bind(on_press = presser)
             self.ids.foodList.add_widget(foodList)
+
+        cursor.close()
+        conn.close()
             
     def reset(self):
         self.ids.food.clear_widgets()
