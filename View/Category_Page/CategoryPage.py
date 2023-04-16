ce990b18258337969f88d4728e9c2a435240281e
Gley Marez <shinsu.0121@gmail.com>
2023-04-16 13:03:28 +0800

    Database Buttons

--- View/Category_Page/CategoryPage.py
+++ View/Category_Page/CategoryPage.py
@@ -2,41 +2,66 @@ from kivy.uix.screenmanager import Screen
 from kivymd.uix.list import TwoLineListItem
 from kivy.lang import Builder
 from kivy.uix.button import Button
-
+from kivy.uix.label import Label
 import pandas as pd
 import sqlite3
-#import ConnectDB
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.gridlayout import GridLayout
+from kivy.properties import ObjectProperty
+#from ConnectDatabase import connect_CerealsDB, connectFoodMix
 
 Builder.load_file('View\Category_Page\CategoryPage.kv')
 
-conn = sqlite3.connect('food_mixtures.db')
 #conn = ConnectDB.connectFoodMix()
-
+#DOUBLE CHECK DATABASE AGAIN
 class CategoryPage(Screen):
     
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        # check if the button is pressed
-        def presser(self):
-            print("Pressed")
+    def presser(self, instance):
+        # do something when a button is pressed
+        print("Button pressed:", instance.text)
 
-        cursor = conn.execute("SELECT foodName FROM FoodMixturesTable")
+    def show_categorypage(self, button):
+        # do something with the button
+        #cursor = None 
+    
+        print("Button pressed:", button)
+        cursor = None
+        data =[]
+        if button == 'cereals_categ':
+            print('Cereals button pressed!')
+            with sqlite3.connect("root_products.db") as conn:
+                print('Database connecting...')
+                cursor = conn.execute("SELECT foodName FROM RootProductsTable")
+                for row in cursor:
+                    data.append(row[0])
+                #self.showDatabase(data)
+                
+        elif button == 'starchy_categ':
+            print('Starchy foods button pressed!')
+            with sqlite3.connect("food_mixtures.db") as conn:
+                print('Database connecting...')
+                cursor = conn.execute("SELECT foodName FROM FoodMixturesTable")
+                for row in cursor:
+                    data.append(row[0])
+                #self.showDatabase(data)
+        print('Creating buttons...')
+        food_list = GridLayout(cols=2, spacing =10, size_hint_y=None)
+        #food_list.clear_widgets() # remove any existing buttons
+        food_list.bind(minimum_height=food_list.setter('height'))
 
-        data = []
-        
-        for row in cursor:
-            data.append(row[0])
+        for i in range(len(data)):
+            btn = Button(text=data[i], size_hint_y=None, height=50, valign='center', font_size=20, background_color = (0,.8,.8,.8))
+            #btn.text_size = (btn.size)
+            food_list.add_widget(btn)
+            print(i)
 
-        for item in data:
-            foodList = Button(text = item, 
-                    size_hint_y = (None),
-                    size = ("50dp", "50dp"))
-            foodList.bind(on_press = presser)
-            self.ids.foodList.add_widget(foodList)
+        root = ScrollView(size_hint=(1,None), size =(400,420))
+        root.add_widget(food_list)
+        return root
 
-        cursor.close()
-        conn.close()
-            
+            #conn.close() 
+
     def reset(self):
         self.ids.food.clear_widgets()
         self.manager.current = "Homepage"
+    
--- View/Home_page/Homepage.py
+++ View/Home_page/Homepage.py
@@ -1,19 +1,46 @@
 from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
-
+from kivy.properties import ObjectProperty
+from View.Category_Page.CategoryPage import CategoryPage
 
 Builder.load_file('View\Home_page\Homepage.kv')
-
+    
 class Homepage(Screen):
     # Icons for each category
+    def buttonIcons(self):
+        self.ids.fruits_icon.source = 'icons/fruits.png'
+        self.ids.veg_icon.source = 'icons/vegetable.png'
+        self.ids.grains_icon.source = 'icons/grains.png'
+        self.ids.protein_icon.source = 'icons/protein.png'
+        self.ids.dairy_icon.source = 'icons/dairy.png'
+        self.ids.more_icon.source = 'icons/more.png'
 
+    # define the presser function to switch to another screen and print "Pressed"
+    #def presser(self, instance):
+        #print("Pressed")
+        # switch to another screen or page
+    def on_button_press(self, button_id):
+        if button_id == 'cereals_categ':
+            button = 'cereals_categ'
+            print("Cereals category button pressed")
+            category_page=CategoryPage()
+            category_page.show_categorypage(button)
+        elif button_id == 'starchy_categ':
+            button = 'starchy_categ'
+            print("Starchy foods category button pressed")
+            category_page=CategoryPage()
+            category_page.show_categorypage(button)
+        
+        self.manager.current = 'CategoryPage'
+        
+    
+ 
+        
     def __init__(self, manager = None, **kwargs):
         self.manager = manager
         super().__init__(**kwargs)
 
     def enter_EditProfilePage(self):
         self.manager.current = "EditProfilePage"
-
-
 
 
