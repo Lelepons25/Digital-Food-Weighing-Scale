91887f53ce7604263702d08d7fffe4edcc40fdc6
Gley Marez <shinsu.0121@gmail.com>
2023-04-23 16:39:43 +0800

    None

--- View/Category_Page/CategoryPage.py
+++ View/Category_Page/CategoryPage.py
@@ -9,62 +9,56 @@ from kivy.uix.scrollview import ScrollView
 from kivy.uix.gridlayout import GridLayout
 from kivy.properties import ObjectProperty
 from kivy.uix.button import Button
-
+from functools import partial
 
 Builder.load_file('View\Category_Page\CategoryPage.kv')
 
 
 class CategoryPage(Screen):
-    foodList = ObjectProperty(None)
-    
-    def presser(self, instance):
-        # do something when a button is pressed
-        print("Button pressed:", instance.text)
-
     def __init__(self, manager, button_id, **kwargs):
         super(CategoryPage, self).__init__(**kwargs)
         self.manager = manager
         self.button_id = button_id
-        cursor = None
-        #rows = []
-
-        if button_id == 'cereals_categ':
+        self.grid_layout = self.ids.foodList
+        self.grid_layout.bind(minimum_height=self.grid_layout.setter('height'))
+        word = ''
+        if self.button_id == 'cereals_categ':
             print('Cereals button pressed!')
-            with sqlite3.connect("root_products.db") as conn:
-                print('Database connecting...')
-                c = conn.cursor()
-                c.execute("SELECT foodName FROM ProductsTable")
-                conn.commit()
-
-        elif button_id == 'starchy_categ':
-            print('Starchy foods button pressed!')
-            with sqlite3.connect("food_mixtures.db") as conn:
-                c = conn.cursor()
-                c.execute("SELECT foodName FROM ProductsTable")
-                
-
-        data = []
-        if cursor is not None:
-            for row in cursor:
-                    data.append(row[0])
-            data = [row[0] for row in cursor]
-
-            print(data)
-            for item in data:
-                food = Button(text = item, 
-                        size_hint_y = (None),
-                        size = ("50dp", "50dp"))
-                food.bind(on_press = self.presser)
+            conn =sqlite3.connect('root_products.db')
+            c = conn.cursor()
+            c.execute("SELECT foodName FROM ProductsTable")
+            records = c.fetchall()
+            print(records)
+            c.close()
+            #word =[tuple(row) for row in rows]
+            for record in records:
+                word = record[0]
+                print(word)
+                food = Button(text=', '.join(list(word)), size_hint_y = (None),
+                        size = ("50dp", "100dp"))
+                print(type(word))
+                self.ids.foodList.bind(on_press=partial(presser, self))
                 self.ids.foodList.add_widget(food)
 
-        if cursor:
-            cursor.close()
+            
+        elif self.button_id == 'starchy_categ':
+            print('Starchy foods button pressed!')
+            with sqlite3.connect('food_mixtures.db') as conn:
+                c = conn.cursor()
+                c.execute("SELECT foodName FROM ProductsTable")
+                records = c.fetchall()
 
-
-
-    
+                for record in records:
+                    word=record[0]
+                    food = Button(text=word, size_hint_y=None, height='50dp')
+                    food.bind(on_press=self.presser)
+                    self.grid_layout.add_widget(food)
+        
 
     def reset(self):
         self.ids.foodList.clear_widgets()
         self.manager.current = "Homepage"
     
+def presser(self, button):
+    # do something when a button is pressed
+    print("Button pressed:", button.text)
