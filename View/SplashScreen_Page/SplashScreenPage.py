93b8c54304e303235e771c922ef59449e2cc586e
Lyll Kristine Cabelin <115082679+Lelepons25@users.noreply.github.com>
2023-05-23 10:34:14 +0800

    Delete View/SplashScreen_Page directory

--- View/SplashScreen_Page/SplashScreenPage.py
+++
@@ -1,7 +0,0 @@
-from kivy.uix.screenmanager import Screen
-from kivy.lang import Builder
-
-Builder.load_file('View\SplashScreen_Page\SplashScreenPage.kv')
-
-class SplashScreenPage(Screen):
-    pass
